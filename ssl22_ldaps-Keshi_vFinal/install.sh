ee0e49a4324f40ea9838533e2bad5a0d971081e1
Debian <admin@ip-172-31-83-13.ec2.internal>
2022-04-19 17:34:03 +0000

    Jennie Tue Apr 19 17:34:03 UTC 2022

--- ssl22_ldaps-Keshi_vFinal/install.sh
+++ ssl22_ldaps-Keshi_vFinal/install.sh
@@ -4,32 +4,21 @@
 # -------------------------------------
 mkdir /etc/ldap/certs
 
-cp /opt/docker/cacert.pem /etc/ldap/certs/. # PER PROVAR-SE A SI MATEIX COM A CLIENT
-cp /opt/docker/cacert.pem /etc/ssl/certs/. # Certificat de CA
-cp /opt/docker/servercert.ldap.pem /etc/ldap/certs/. # Certificat del servidor
-cp /opt/docker/servercertPLUS.pem /etc/ldap/certs/. # Certificat PLUS del servidor
-cp /opt/docker/serverkey.ldap.pem  /etc/ldap/certs/. # Key del servidor
+cp /opt/docker/ca_leomessi_cert.pem /etc/ldap/certs/. # PER PROVAR-SE A SI MATEIX COM A CLIENT
+cp /opt/docker/ca_leomessi_cert.pem /etc/ssl/certs/. # Certificat de CA
+cp /opt/docker/servercert_ldap.pem /etc/ldap/certs/. # Certificat del servidor
+
+cp /opt/docker/serverkey_ldap.pem  /etc/ldap/certs/. # Key del servidor
 
 rm -rf /etc/ldap/slapd.d/*
 rm -rf /var/lib/ldap/*
 
-cp /opt/docker/slapd.conf /etc/ldap/slapd.conf 
+cp /opt/docker/slapd.conf /etc/ldap/slapd.conf
 
 slaptest -f /opt/docker/slapd.conf -F /etc/ldap/slapd.d # Provem i ensamblem el servidor LDAP
 slaptest -u -f /opt/docker/slapd.conf -F /etc/ldap/slapd.d
 
-slapadd -F /etc/ldap/slapd.d -l /opt/docker/edt.org.ldif
+slapadd -F /etc/ldap/slapd.d -l /opt/docker/edt.org.ldif # Populate de LDAP
 chown -R openldap.openldap /etc/ldap/slapd.d /var/lib/ldap
 
-cp /opt/docker/ldap.conf /etc/ldap/ldap.conf # PER PROBAR-SE A SI MATEIX
-
-
-########EXTRA ALTERNATE############
-
-#A /etc/ssl/openssl.cnf li hem d'indicar ext.alternate.conf
-#Si volguesim tenir sinonims del host (amb CA valid)  hauriem d'haver creat el certificat del ServerLdap incoporant ext.alternate.conf
-
-#openssl x509 -CA cacert.pem -CAkey cakey.pem -req -in serverrequest.ldap.pem -out servercert.pem -CAcreateserial -extfile ext.alternate.conf -days 360
-
-###################################
-
+cp /opt/docker/ldap.conf /etc/ldap/ldap.conf # Necesari per probar-se a si mateix
